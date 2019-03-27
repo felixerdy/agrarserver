cf6e2bb434f7aab5fa53d86f63d9c867792657ea
felixerdy <felix.erdmann@uni-muenster.de>
2019-03-27 12:03:13 +0100

    remove unused WPS request code

--- web/Map.js
+++ web/Map.js
@@ -15,59 +15,6 @@ L.Icon.Default.mergeOptions({
 const baseUrl = "http://localhost:8080";
 const flaskURL = "http://localhost:5000";
 
-const gatewayURL = "http://localhost:5000";
-
-const geometryIntersectXML = parameter => `<?xml version="1.0" encoding="UTF-8"?><wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
-<ows:Identifier>gs:IdentifyOverlaps</ows:Identifier>
-<wps:DataInputs>
-<wps:Input>
-<ows:Identifier>polygon</ows:Identifier>
-<wps:Data>
-  <wps:ComplexData mimeType="application/json">
-    <![CDATA[
-${parameter}
-]]>
-  </wps:ComplexData>
-</wps:Data>
-</wps:Input>
-</wps:DataInputs>
-<wps:ResponseForm>
-<wps:RawDataOutput mimeType="application/json">
-<ows:Identifier>result</ows:Identifier>
-</wps:RawDataOutput>
-</wps:ResponseForm>
-</wps:Execute>`;
-
-const geometryInsertXML = coordinateList => `<?xml version="1.0"?>
-<wfs:Transaction
-   version="2.0.0"
-   service="WFS"
-   xmlns:fes="http://www.opengis.net/fes/2.0"
-   xmlns:gml="http://www.opengis.net/gml/3.2"
-   xmlns:wfs="http://www.opengis.net/wfs/2.0"
-   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-   xsi:schemaLocation="http://www.opengis.net/wfs/2.0
-                       http://schemas.opengis.net/wfs/2.0/wfs.xsd
-                       http://www.opengis.net/gml/3.2
-                       http://schemas.opengis.net/gml/3.2.1/gml.xsd">
-   <wfs:Insert>
-    <parcels>
-      <geometry>
-        <gml:Polygon>
-         <gml:exterior>
-            <gml:LinearRing>
-               <gml:posList>
-              ${coordinateList.flat().join(" ")}
-            	</gml:posList>
-            </gml:LinearRing>
-         </gml:exterior>
-      </gml:Polygon>
-      </geometry>
-    </parcels>
-    <!-- you can insert multiple features if you wish-->
-   </wfs:Insert>
-</wfs:Transaction>`;
-
 export default () => {
   var map = L.map("map", {
     drawControl: true
