caa8b1db02151a09e8bf4c25371a6b723c33c0ae
felixerdy <felix.erdmann@uni-muenster.de>
2019-04-08 11:35:18 +0200

    include all states in UI

---
+++ web/src/index.js
@@ -0,0 +1,9 @@
+import Map from "./Map";
+
+window.onload = () => {
+    const stateName = process.env.GATEWAY_HOST.split('.')[1]
+    document.getElementById("state_name").textContent = stateName
+    document.title = stateName
+}
+
+Map();
